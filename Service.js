ab2fe15b1bd8369035f6d7dcbf3e628d1a622852
trishadeepak12 <trishasudarsan12@gmail.com>
2023-05-08 11:25:43 +0530

    initial is done

--- Service.js
+++ Service.js
@@ -1,6 +1,37 @@
 const express=require('express')
 const router=express.Router()
 router.get('/',(req,res)=>{
-    res.send("Service Page")
+  res.send("Service Page")
 })
-module.exports=router
+
+router.get('/:id([0-9])',(req,res)=>{
+
+  if(req.params.id<=ServiceArrayLength){
+    res.send(`Service Name for this id ${service[req.params.id-1].idno} is ${service[req.params.id-1].name}`)
+  }
+  else{
+    res.send("Wrong id")
+  }
+  // res.send(`List of Service ${req.params.id}`)
+})
+
+
+const service=[{
+  idno:1,
+  name:'mern'
+},
+{
+  idno:2,
+  name:'java'
+},
+{
+  idno:3,
+  name:'web development'
+},
+{
+  idno:4,
+  name:'express'
+}]
+
+const ServiceArrayLength=service.length;
+module.exports=router;
